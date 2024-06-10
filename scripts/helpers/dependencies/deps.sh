b1b203ff78903ceddcabf48021d4de27b1e1691a
vanely <vanelys94@gmail.com>
2024-06-10 03:11:59 -0400

    adding some plugins

--- scripts/helpers/dependencies/deps.sh
+++ scripts/helpers/dependencies/deps.sh
@@ -18,11 +18,42 @@ install_ripgrep() {
     elif [[ "${OSTYPE}" == "darwin" ]]; then
       # install for mac
       brew install ripgrep
+      echo "---------------------------------------------"
     elif [[ "${OSTYPE}" == "msys" ]]; then
       # install for windows(Git Bash)
       powershell scoop_install.ps1
       source ~/.bashrc
       scoop install ripgrep
+      echo "---------------------------------------------"
+      echo "Usupported operating system."
+    fi
+  fi
+}
+
+
+install_ack-grep() {
+  if [[ -n "$(which ripgrep)" ]]; then
+    echo "ack-grep is already installed on this machine."
+    echo "---------------------------------------------"
+    echo
+  else
+    echo "Installing ack-grep"
+    echo "---------------------------------------------"
+    echo
+    if [[ "${OSTYPE}" == "linux-gnu" ]]; then
+      # install for linux(ubuntu/debian)
+      sudo apt update -y
+      sudo apt install ack-grep -y
+      echo "---------------------------------------------"
+      echo
+    elif [[ "${OSTYPE}" == "darwin" ]]; then
+      # install for mac
+      brew install ack
+      echo "---------------------------------------------"
+    elif [[ "${OSTYPE}" == "msys" ]]; then
+      # install for windows(Git Bash)
+      scoop install ack
+      echo "---------------------------------------------"
     else
       echo "Usupported operating system."
     fi
