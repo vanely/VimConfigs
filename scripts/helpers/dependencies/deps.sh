76994fe4913b95b47f3fd916b8aa6bb7b794f2c3
vanely <vanelys94@gmail.com>
2024-06-09 20:55:03 -0400

    made ripgrep install os agnostic

--- scripts/helpers/dependencies/deps.sh
+++ scripts/helpers/dependencies/deps.sh
@@ -9,9 +9,22 @@ install_ripgrep() {
     echo "Installing ripgrep."
     echo "---------------------------------------------"
     echo
-    sudo apt update -y
-    sudo apt install ripgrep -y
-    echo "---------------------------------------------"
-    echo
+    if [[ "${OSTYPE}" == "linux-gnu" ]]; then
+      # install for linux(ubuntu/debian)
+      sudo apt update -y
+      sudo apt install ripgrep -y
+      echo "---------------------------------------------"
+      echo
+    elif [[ "${OSTYPE}" == "darwin" ]]; then
+      # install for mac
+      brew install ripgrep
+    elif [[ "${OSTYPE}" == "msys" ]]; then
+      # install for windows(Git Bash)
+      powershell scoop_install.ps1
+      source ~/.bashrc
+      scoop install ripgrep
+    else
+      echo "Usupported operating system."
+    fi
   fi
 }
