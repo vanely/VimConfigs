fc9e82ca46292664d5013efbca05382a95ac96db
vanely <vanelys94@gmail.com>
2024-11-19 00:02:35 -0500

    lots of keymaps

--- configs/personal/neovim/init.lua
+++ configs/personal/neovim/init.lua
@@ -12,11 +12,6 @@ vim.opt.tabstop = 2
 vim.opt.scrolloff = 8
 vim.opt.updatetime = 250
 
--- key maps and re-maps 
-vim.keymap.set('i', 'jj', '<Esc>', { desc = "Exit insert mode with jj" }) 
--- vim.keymap.set('n', 'jj', '<Esc>', { desc = "General escape in normal mode" })
-vim.keymap.set('n', '<Esc>', ':noh<CR>', {silent = true, desc = "Clear search highlight" })
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -97,13 +92,62 @@ local plugins = {
         },
       })
     end
+  },
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    dependencies = { 'nvim-lua/plenary.nvim'},
   }
 }
 local opts = {}
 require("lazy").setup(plugins, opts)
 
 -- themes
--- vim.cmd.colorscheme("catppuccin")
+vim.cmd.colorscheme("catppuccin")
 -- vim.cmd.colorscheme("sonokai")
 -- vim.cmd.colorscheme("everforest")
- vim.cmd.colorscheme("nordic")
+-- vim.cmd.colorscheme("nordic")
+
+----------------------------------------------------------------------------------------------------------------------
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+-- key maps and re-maps 
+vim.keymap.set('i', 'jj', '<Esc>', { desc = "Exit insert mode with jj" }) 
+-- vim.keymap.set('n', 'jj', '<Esc>', { desc = "General escape in normal mode" })
+vim.keymap.set('n', '<Esc>', ':noh<CR>', { silent = true, desc = "Clear search highlight" })
+
+-- save and quit operations
+vim.keymap.set('n', '<leader>w', ':w<CR>', { desc = "Save file" })
+vim.keymap.set('n', '<leader>q', ':q<CR>', { desc = "Quit" })
+vim.keymap.set('n', '<leader>Q', ':qa!<CR>', { desc = "Force quit all" })
+vim.keymap.set('n', '<leader>x', ':wq<CR>', { desc = "Save and quit" })
+
+-- buffer operations
+vim.keymap.set('n', '<leader>bd', ':bdelete<CR>', { desc = "Delete buffer" })
+vim.keymap.set('n', '<leader>l', ':bnext<CR>', { desc = "Next buffer" })
+vim.keymap.set('n', '<leader>h', ':bprevious<CR>',{ desc = "Previous buffer" })
+
+-- window operations
+vim.keymap.set('n', '<leader>vs', ':vsplit<CR>', { desc = "Vertical split" })
+vim.keymap.set('n', '<leader>hs', ':split<CR>', { desc = "Horizontal split" })
+vim.keymap.set('n', '<leader>sx', ':close<CR>', { desc = "Close current split" })
+vim.keymap.set('n', '<C-h>', '<C-W>h', { desc = "Move to left window" })
+vim.keymap.set('n', '<C-l>', '<C-W>l', { desc = "Move to right window" })
+vim.keymap.set('n', '<C-j>', '<C-W>j', { desc = "Move to bottom window" })
+vim.keymap.set('n', '<C-k>', '<C-W>k', { desc = "Move to window above" })
+
+-- file navigation
+vim.keymap.set('n', '<leader>/', ':Telescope live_grep<CR>', { desc = "Search in files" })
+vim.keymap.set('n', '<C-p>', ':Telescope find_files<CR>', { desc = "Find files" })
+vim.keymap.set('n', '<C-f>', ':Telescope buffers<CR>', { desc = "Find buffers" })
+vim.keymap.set('n', '<leader>fh', ':Telescope help_tags', { desc = "Find help" })
+
+-- copy&past improvements
+vim.keymap.set('n', '<leader>y', '"+y', { desc = "Copy to system clipboard" })
+vim.keymap.set('v', '<leader>y', '"+y', { desc = "Copy to system clipboard" })
+vim.keymap.set('n', '<leader>p', '"+p', { desc = "Paste from system clipboard" })
+vim.keymap.set('v', '<leader>p', '"+p', { desc = "Paste from system clipboard" })
+
+
+
+
+ 
